d93477ff080e050605513d788f6900b89fc79676
Jiahui-Ruan <jr58@rice.edu>
2016-09-07 00:05:03 -0500

    first_commit

---
+++ inclass-5/avoid.js
@@ -0,0 +1,41 @@
+	//design the pattern of button escaping
+	var escape = function(){
+		var docHeight = $(document).height(),
+	        docWidth = $(document).width(),
+	        $button = $('#btn'),
+	        btnWidth = $button.width(),
+	        btnHeight = $button.height(),
+	        heightMax = docHeight - btnHeight,
+	        widthMax = docWidth - btnWidth;
+	    //change the postion of the button by css
+	    $button.css({
+	        left: Math.floor( Math.random() * widthMax ),
+	        top: Math.floor( Math.random() * heightMax )
+	    });
+	}
+	//when the mouse approch the button, it escapes
+	$('#btn').hover(function(e) {
+		if(e.shiftKey) {
+			return false;
+		}
+		escape();
+	});
+	//there are two state when the btn is clicked
+	$('#btn').click(function() {
+		var state = document.getElementById('btn');
+		var congrats = document.getElementById('hidden');
+
+		if(state.innerHTML == "Click Me") {
+			congrats.style.display = 'inline';
+			state.innerHTML = "Play Again";
+		}
+		else if(state.innerHTML == "Play Again") {
+			congrats.style.display = 'none';
+			//start over the game
+			escape();
+			state.innerHTML = "Click Me";
+		}
+	});
+	$('#tips').click(function() {
+		alert("press shiftKey to stop the button.");
+	})
