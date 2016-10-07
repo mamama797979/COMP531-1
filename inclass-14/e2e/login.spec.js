b790c693337a8c6f47b4bd4232d3573d8cc7102e
Jiahui-Ruan <jr58@rice.edu>
2016-10-06 23:18:32 -0500

    inclass-14

---
+++ inclass-14/e2e/login.spec.js
@@ -0,0 +1,56 @@
+import { expect } from 'chai'
+import { go, sleep, findId, findCSS, By } from './selenium'
+import common from './common'
+
+describe('Test Dummy Server Example Page', () => {
+
+    const preamble = 'you are logged in as'
+
+    before('should log in', (done) => {
+        go().then(common.login).then(done)
+    })
+
+    it('should log in as the test user', (done) => {
+        sleep(500)
+        .then(findId('message').getText()
+            .then(text => {
+                expect(text.indexOf(preamble)).to.equal(0)
+            })
+            .then(done))
+    })
+
+    it("Update the headline and verify the change", (done) => {
+        // IMPLEMENT ME
+        var username = common.creds.username;
+        var prefix = `you are logged in as ${username} `
+        var oldHeadline = 'old headline!'
+        var newHeadline = 'new headline!'
+        sleep(500)
+        // find the headline input
+        .then(findId('newHeadline').sendKeys(newHeadline))
+        .then(findId('headline').click())
+        .then(sleep(2000))
+        // .sendKeys(new headline message)
+        // verify the headline is updated
+        .then(findId('message').getText()
+            .then(text => {
+                expect(text).to.equal(prefix + '"' + newHeadline + '"')
+            }))
+        // .sendKeys(the old headline message)
+        .then(sleep(2000))
+        .then(findId('newHeadline').clear())
+        .then(findId('newHeadline').sendKeys(oldHeadline))        
+        .then(findId('headline').click())
+        .then(sleep(2000))
+        // verify the headline is updated
+        .then(findId('message').getText()
+            .then(text => {
+                expect(text).to.equal(prefix + '"' + oldHeadline + '"')
+            })
+            .then(done))
+    })
+
+    after('should log out', (done) => {
+        common.logout().then(done)
+    })
+})
